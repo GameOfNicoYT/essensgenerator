699753c8d8c57d7b51e13e42aa95c3f2592a0422
GameOfNicoYT <nicoproyer@gmail.com>
2022-12-17 14:58:31 +0100

    Updated Duplicate Dishes plus made a list for clear reading

--- index.js
+++ index.js
@@ -4,32 +4,94 @@ let hauptspeisen = ['Carbonara', 'Nudelauflauf', 'Wrap', 'Schnitzel', 'Gordon Bl
 let nachspeisen = ['Germknödl', 'Reisauflauf', 'Kaiserschmarren', 'Scheiterhaufen', 'Mohnnudeln']
 let restaurant = ['Sajado', 'Bohmann', 'Buschenschank', 'Heinz', 'Kellerschlössl', 'Goldino', 'McDonalds', 'Giovanni', 'Losteria']
 let auswahl;
+let storage;
 
-function chooseDish(type){
+function allStorage() {
 
-    if(type == "all"){
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while (i--) {
+        values.push(localStorage.getItem(keys[i]));
+    }
+
+    return values;
+}
+
+function chooseDish(type) {
+
+    storage = allStorage();
+
+    if (type == "all") {
+        for (i = 0; i < storage.length; i++) {
+            storage[i] = storage[i] - 100;
+        }
+        console.log(storage)
         var max = liste.length;
-        var x = Math.round(Math.random() * (max - 1 - 0))
+        var x = Math.floor(Math.random() * max)
+        while (storage.includes(x)) {
+            var max = liste.length;
+            var x = Math.floor(Math.random() * max)
+        }
         document.getElementById("gericht").value = liste[x];
+        window.localStorage.setItem(Date.now(), 100 + x)
     }
-    if(type == "pre"){
+    if (type == "pre") {
+        for (i = 0; i < storage.length; i++) {
+            storage[i] = storage[i] - 200;
+        }
+        console.log(storage)
         var max = vorspeisen.length;
-        var x = Math.round(Math.random() * (max - 1 - 0))
-        document.getElementById("gericht").value = vorspeisen[x];
+        var x = Math.floor(Math.random() * max)
+        while (storage.includes(x)) {
+            var max = vorspeisen.length;
+            var x = Math.floor(Math.random() * max)
+        }
+        document.getElementById("gericht").value = liste[x];
+        window.localStorage.setItem(Date.now(), 200 + x)
     }
-    if(type == "main"){
+    if (type == "main") {
+        for (i = 0; i < storage.length; i++) {
+            storage[i] = storage[i] - 300;
+        }
+        console.log(storage)
         var max = hauptspeisen.length;
-        var x = Math.round(Math.random() * (max - 1 - 0))
+        var x = Math.floor(Math.random() * max)
+        while (storage.includes(x)) {
+            var max = hauptspeisen.length;
+            var x = Math.floor(Math.random() * max)
+        }
         document.getElementById("gericht").value = hauptspeisen[x];
+        window.localStorage.setItem(Date.now(), 300 + x)
     }
-    if(type == "after"){
+    if (type == "after") {
+        for (i = 0; i < storage.length; i++) {
+            storage[i] = storage[i] - 400;
+        }
+        console.log(storage)
         var max = nachspeisen.length;
-        var x = Math.round(Math.random() * (max - 1 - 0))
+        var x = Math.floor(Math.random() * max)
+        while (storage.includes(x)) {
+            var max = nachspeisen.length;
+            var x = Math.floor(Math.random() * max)
+        }
         document.getElementById("gericht").value = nachspeisen[x];
+        window.localStorage.setItem(Date.now(), 400 + x)
     }
-    if(type == "res"){
+    if (type == "res") {
+        for (i = 0; i < storage.length; i++) {
+            storage[i] = storage[i] - 500;
+        }
+        console.log(storage)
         var max = restaurant.length;
-        var x = Math.round(Math.random() * (max - 1 - 0))
+        var x = Math.floor(Math.random() * max)
+        while (storage.includes(x)) {
+            var max = restaurant.length;
+            var x = Math.floor(Math.random() * max)
+        }
         document.getElementById("gericht").value = restaurant[x];
+        window.localStorage.setItem(Date.now(), 500 + x)
     }
 }
+
